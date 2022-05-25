87768fcfc822490be23efca04fa9cc194fea0292
Fabio <fabio_d89@hotmail.com>
2022-05-25 18:41:22 -0400

    Third commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,40 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import test_data from "./test_data"
 
-function KBCard(props) {return(<>
-    <Card>
-        <Card.Title>{props.Title}</Card.Title>
-        <Card.Body>{props.Content}</Card.Body>
+function KBCard(props) {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);    
+    return(<div>
+
+    <Card className="card" onClick={handleShow} cursor="pointer">
+        <Card.Body>{props.Title}</Card.Body>
     </Card>
-</>)
+
+      <Modal show={show} onHide={handleClose} size="lg" scrollable="true" fullscreen="true" aria-labelledby="contained-modal-title-vcenter"
+      centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.Title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.Content}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+</div>)
 }
 
 function App (){
     let [searchTerm, setSearch] = useState("")
     let results = test_data.map((x) => 
     {if ((x.Title.toLowerCase()).includes(searchTerm.toLowerCase()))
-        return <p>{<KBCard Title={x.Title} Content={x.Content}/>}</p>})
+        return <>{<KBCard Title={x.Title} Content={x.Content}/>}</>})
 
     return (<>
         <input placeholder="Search here..." onChange={(x)=> {setSearch(searchTerm = x.target.value)}}/>
